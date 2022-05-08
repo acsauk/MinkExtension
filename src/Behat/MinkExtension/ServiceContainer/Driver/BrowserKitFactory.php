e6ee36265a11b2c310229a93795906e4376169b1
Alex Saunders <acsauk@gmail.com>
2022-05-08 15:19:58 +0100

    Inject rather than sety

--- src/Behat/MinkExtension/ServiceContainer/Driver/BrowserKitFactory.php
+++ src/Behat/MinkExtension/ServiceContainer/Driver/BrowserKitFactory.php
@@ -65,10 +65,9 @@ class BrowserKitFactory implements DriverFactory
             throw new \RuntimeException(sprintf('Class %s not found, did you install symfony/browser-kit 4.4+?', HttpBrowser::class));
         }
 
-        $parameters['$defaultOptions'] = $config['http_client_parameters'] ?? [];
+        $parameters[] = $config['http_client_parameters'] ?? [];
 
-        $httpClientDefinition = new Definition(NativeHttpClient::class);
-        $httpClientDefinition->addMethodCall('create', $parameters);
+        $httpClientDefinition = new Definition(NativeHttpClient::class, $parameters);
 
         return new Definition(BrowserKitDriver::class, [
             new Definition(HttpBrowser::class, [
